160fb68fb0ef30efb171ebe7d55e69bd4f24cfd8
GachiBoy0706 <sazhin.dd@edu.spbstu.ru>
2024-04-16 22:07:25 +0300

    added new folders and so on

--- lab_4/main.py
+++ lab_4/main.py
@@ -62,11 +62,10 @@ def drawInfoPoisson(array_of_dispersion, array_of_mathematical_expectation, powe
 
 def calculate_normal(quantiles, powers):
     print("normal_distribution:")
-    bins = [10, 20]
+    bins = [10, 18]
     array_of_dispersion = []
     array_of_mathematical_expectation = []
     for power, quantile, bin in zip(powers, quantiles, bins):
-
         normal = np.random.normal(0, 1, power)
 
         average = np.mean(normal)
@@ -90,20 +89,56 @@ def calculate_normal(quantiles, powers):
         array_of_mathematical_expectation.append(LeftMathematicalExpectation)
         array_of_mathematical_expectation.append(RightMathematicalExpectation)
 
-        # Создаем гистограмму
+        # Создаем гистограмму с заполненными столбцами
         plt.figure(figsize=(8, 6))
-        sns.histplot(normal, bins=bin, kde=True, stat="density")
+        bin_values, bin_edges, _ = plt.hist(normal, bins=bin, density=True, alpha=0.7, edgecolor='black')
+
+        # Замена пустых столбцов на столбцы с высотой 1
+        for i in range(len(bin_values)):
+            if bin_values[i] == 0:
+                bin_values[i] = (bin_values[i - 1] + bin_values[i + 1]) / 2
+
+        # Рисуем заполненные столбцы
+        plt.bar(bin_edges[:-1], bin_values, width=np.diff(bin_edges), align='edge', alpha=0.7, edgecolor='black', color='cyan')
+
+        # Добавляем график аппроксимации
+        sns.kdeplot(normal, color='red')
+
         plt.title(f'{"normal"}, size {power}', fontsize=18)
         plt.xlabel('Values', fontsize=14)
         plt.ylabel('Density', fontsize=14)
 
         plt.axvline(x=LeftMathematicalExpectation - RightDispersion, color='purple', linewidth=2, clip_on=False, label='min \mu - max\sigma')
+        plt.annotate(f'{LeftMathematicalExpectation - RightDispersion:.2f}', 
+             xy=(LeftMathematicalExpectation - RightDispersion, 0), 
+             xytext=(LeftMathematicalExpectation - RightDispersion, -0.02), 
+             ha='center', 
+             fontsize=10)
         plt.plot(LeftMathematicalExpectation - RightDispersion, 0, marker='o', markersize=10, color='purple')
+
         plt.axvline(x=RightMathematicalExpectation + RightDispersion, color='purple', linewidth=2, clip_on=False, label='max \mu + max\sigma')
+        plt.annotate(f'{RightMathematicalExpectation + RightDispersion:.2f}', 
+             xy=(RightMathematicalExpectation + RightDispersion, 0), 
+             xytext=(RightMathematicalExpectation + RightDispersion, -0.02), 
+             ha='center', 
+             fontsize=10)
         plt.plot(RightMathematicalExpectation + RightDispersion, 0, marker='o', markersize=10, color='purple')
+
         plt.axvline(x=LeftMathematicalExpectation, color='red', linewidth=2, clip_on=False, label='min \mu')
+        plt.annotate(f'{LeftMathematicalExpectation:.2f}', 
+             xy=(LeftMathematicalExpectation, 0), 
+             xytext=(LeftMathematicalExpectation, -0.02), 
+             ha='center', 
+             fontsize=10)
         plt.plot(LeftMathematicalExpectation, 0, marker='o', markersize=10, color='red')
+
+
         plt.axvline(x=RightMathematicalExpectation, color='red', linewidth=2, clip_on=False, label='max \mu')
+        plt.annotate(f'{RightMathematicalExpectation:.2f}', 
+             xy=(RightMathematicalExpectation, 0), 
+             xytext=(RightMathematicalExpectation, -0.02), 
+             ha='center', 
+             fontsize=10)
         plt.plot(RightMathematicalExpectation, 0, marker='o', markersize=10, color='red')
         plt.legend()
         plt.savefig(f'{"normal"}, size {power}.png')
@@ -113,7 +148,7 @@ def calculate_normal(quantiles, powers):
 
 def pyasson_distribution(quantiles, powers):
     print("poisson_distribution:")
-    bins = [10, 20]
+    bins = [10, 18]
     array_of_dispersion = []
     array_of_mathematical_expectation = []
     for power, bin in zip(powers, bins):
@@ -145,21 +180,59 @@ def pyasson_distribution(quantiles, powers):
         array_of_mathematical_expectation.append(LeftMathematicalExpectation)
         array_of_mathematical_expectation.append(RightMathematicalExpectation)
 
-        # Создаем гистограмму
+
+        # Создаем гистограмму с заполненными столбцами
         plt.figure(figsize=(8, 6))
-        sns.histplot(poisson, bins=bin, kde=True, stat="density")
+        bin_values, bin_edges, _ = plt.hist(poisson, bins=bin, density=True, alpha=0.7, edgecolor='black')
+
+        # Замена пустых столбцов на столбцы с высотой 1
+        for i in range(len(bin_values)):
+            if bin_values[i] == 0:
+                bin_values[i] = (bin_values[i - 1] + bin_values[i + 1]) / 2
+
+        # Рисуем заполненные столбцы
+        plt.bar(bin_edges[:-1], bin_values, width=np.diff(bin_edges), align='edge', alpha=0.7, edgecolor='black', color='cyan')
+
+        # Добавляем график аппроксимации
+        sns.kdeplot(poisson, color='red')
+
         plt.title(f'{"poisson"}, size {power}', fontsize=18)
         plt.xlabel('Values', fontsize=14)
         plt.ylabel('Density', fontsize=14)
 
         plt.axvline(x=LeftMathematicalExpectation - RightDispersion, color='purple', linewidth=2, clip_on=False, label='min \mu - max\sigma')
+        plt.annotate(f'{LeftMathematicalExpectation - RightDispersion:.2f}', 
+             xy=(LeftMathematicalExpectation - RightDispersion, 0), 
+             xytext=(LeftMathematicalExpectation - RightDispersion, -0.02), 
+             ha='center', 
+             fontsize=10)
         plt.plot(LeftMathematicalExpectation - RightDispersion, 0, marker='o', markersize=10, color='purple')
+
         plt.axvline(x=RightMathematicalExpectation + RightDispersion, color='purple', linewidth=2, clip_on=False, label='max \mu + max\sigma')
+        plt.annotate(f'{RightMathematicalExpectation + RightDispersion:.2f}', 
+             xy=(RightMathematicalExpectation + RightDispersion, 0), 
+             xytext=(RightMathematicalExpectation + RightDispersion, -0.02), 
+             ha='center', 
+             fontsize=10)
         plt.plot(RightMathematicalExpectation + RightDispersion, 0, marker='o', markersize=10, color='purple')
+
         plt.axvline(x=LeftMathematicalExpectation, color='red', linewidth=2, clip_on=False, label='min \mu')
+        plt.annotate(f'{LeftMathematicalExpectation:.2f}', 
+             xy=(LeftMathematicalExpectation, 0), 
+             xytext=(LeftMathematicalExpectation, -0.02), 
+             ha='center', 
+             fontsize=10)
         plt.plot(LeftMathematicalExpectation, 0, marker='o', markersize=10, color='red')
+
+
         plt.axvline(x=RightMathematicalExpectation, color='red', linewidth=2, clip_on=False, label='max \mu')
+        plt.annotate(f'{RightMathematicalExpectation:.2f}', 
+             xy=(RightMathematicalExpectation, 0), 
+             xytext=(RightMathematicalExpectation, -0.02), 
+             ha='center', 
+             fontsize=10)
         plt.plot(RightMathematicalExpectation, 0, marker='o', markersize=10, color='red')
+        plt.legend()
 
         plt.savefig(f'{"poisson"}, size {power}.png')
     
@@ -178,5 +251,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
